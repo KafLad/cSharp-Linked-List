75d6979a04d9e1f7a8308e75ee9bdec1c29388c4
Ethan Gray <74423016+KafLad@users.noreply.github.com>
2022-03-08 09:32:30 -0800

    Initial Library Version
    Ability to have a functioning Linked List w/ Insertion Sort capabilities

---
+++ MyListLibrary/MyListLibrary.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System;
+
+namespace MyListLibrary
+{
+
+    public class MyList<T> : IEnumerable<T> where T: IComparable
+    {
+
+        private class Node<T>
+        {
+            public T data;
+            public Node<T> next;
+            public Node()
+            {
+                data = default;
+                next = null;
+            }
+            public Node(T data, Node<T> next)
+            {
+                this.data = data;
+                this.next = next;
+            }
+            public override string ToString()
+            {
+                return $"[Node with {data} and points to {next}]";
+            }
+        } // End of Node class
+        
+        // Beginning of Linked List
+        private Node<T> head;
+        private Node<T> tail;
+        private int count;
+        public int Count => count;
+
+        public MyList()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public MyList(T[] arr)
+        {
+            foreach(T item in arr)
+            {
+                Add(item);
+            }
+        }
+
+        public void Add(T item)
+        {
+            if(head == null)
+            {
+            head = new Node<T>(item, null);
+            tail = head;
+            }
+            else
+            {
+            Node<T> new_tail = new Node<T>(item, null);
+            tail.next = new_tail;
+            tail = new_tail;
+            }
+            count++;
+        }
+
+        public int IndexOf(T item, int startAt = 0)
+        {
+            int idx = startAt;
+            Node<T> curr = WalkToIndex(idx);
+            while(curr != null)
+            {
+                if(item.CompareTo(curr.data) == 0)
+                {
+                return idx;
+                }
+                curr = curr.next;
+                idx++;
+            }
+            return -1;
+        }
+
+        private Node<T> WalkToIndex(int index)
+        {
+            Node<T> curr = head;
+            if ( (index < 0) || (index > count))
+            {
+            throw new IndexOutOfRangeException($"Error: index {index} is out of range!");
+            }
+            for(int i = 0; i < index; i++)
+            {
+                curr = curr.next;
+            }
+            return curr;
+        }
+
+        public void Insert(T item, int index)
+        {
+            Node<T> prev = WalkToIndex(index-1);
+            Node<T> node = new Node<T>(item, prev.next);
+            prev.next = node;
+            count++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            Node<T> del = WalkToIndex(index);
+            Node<T> delPrev = WalkToIndex(index - 1);
+            delPrev.next = del.next;
+            del = null;
+            count--;
+        }
+
+        public bool Contains(T item)
+        {
+            Node<T> curr = head;
+            while(curr != null)
+            {
+                if(item.CompareTo(curr.data) == 0)
+                {
+                    return true;
+                }
+                curr = curr.next;
+            }
+            return false;
+        }
+        
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public T[] ToArray()
+        {
+            T[] arr = new T[count];
+            Node<T> node = head;
+            for (int i = 0; i < count; i++)
+            {
+                node = WalkToIndex(i);
+                arr[i] = node.data;
+            }
+            return arr;
+        }
+
+        private void Shuffle(int indexOne, int indexTwo)
+        {
+            T temp = this[indexOne];
+            this[indexOne] = this[indexTwo];
+            this[indexTwo] = temp;
+        }
+
+        public void SimpleSort()
+        {
+            for(int i = 0; i < count; i++)
+            {
+                int mindex = i;
+                for(int j = i; j < count; j++)
+                {
+                    if(this[j].CompareTo(this[mindex]) < 0)
+                    {
+                        mindex = j;
+                    }
+                }
+                Shuffle(i, mindex);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> curr = head;
+            for(int i = 0; i < count; i++)
+            {
+                yield return curr.data;
+                if(curr.next != null) { curr = curr.next; }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        
+        public override string ToString()
+        {
+            string type = head.data.GetType().Name;
+            string msg = $"MyList Object type '{type}' | Length: {count}";
+            return msg;
+        }
+        public T this[int index]
+        {
+            get
+            {
+                Node<T> node = WalkToIndex(index);
+                return node.data;
+            }
+            set
+            {
+                Node<T> node = WalkToIndex(index);
+                node.data = value;
+            }
+
+        }
+
+    }
+}
---
+++ MyListProgram/MyListProgram.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using MyListLibrary;
+namespace MyListProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("#== MyList Test Program ==#");
+            MyList<int> list = new MyList<int>();
+            Random rand = new Random();
+            for(int i = 0; i < 15; i++)
+            {
+                int num = rand.Next(100);
+                list.Add(num);
+            }
+
+            // Testing Enumeration
+            Console.WriteLine(list);
+            Console.WriteLine("\n#== Enumerating Test ==#");
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Testing IndexOf() Function
+            Console.WriteLine("\n#== IdexOf() Test ==#");
+            Console.WriteLine($"Testing for index of int 5: {list.IndexOf(5) }");
+            Console.WriteLine($"Testing for index of int 100: {list.IndexOf(100) }");
+
+            // Testing Contains() Function
+            Console.WriteLine("\n#== Contains() Test ==#");
+            Console.WriteLine($"Checking if list contains int 14: {list.Contains(14) }");
+            Console.WriteLine($"Checking if list contains int 100: {list.Contains(100) }");
+
+            // Testing Count variable 
+            Console.WriteLine("\n#== Count value test ==#");
+            Console.WriteLine($"List Count vale: {list.Count}");
+
+            // Testing Insert() Function
+            Console.WriteLine("\n#== Insert() Test ==#");
+            Console.WriteLine("Inserting '17' at index '2'");
+            list.Insert(17, 2);
+            Console.WriteLine("New List:");
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Testing RemoveAt() Function
+            Console.WriteLine("\n#== RemoveAt() Test ==#");
+            Console.WriteLine("Removing '17' at index '2'");
+            list.RemoveAt(2);
+            Console.WriteLine("New List:");
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Testing get-set with [] notation
+            Console.WriteLine("\n#== Index Get-Set Test ==#");
+            Console.WriteLine("Setting '17' to index '2' using '[int index]' notation");
+            list[2] = 17;
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"List index {i} = {list[i]}");
+            }
+            Console.WriteLine("Previous 3 indexes called using '[int index]' notation.");
+
+            // Testing ToArray() Function
+            Console.WriteLine("\n#== ToArray() Test ==#");
+            Console.WriteLine("Converting MyList object to array.");
+            int[] arr = list.ToArray();
+            Console.Write("[ ");
+            foreach(int i in arr)
+            {
+                Console.Write($"{i}, ");
+            }
+            Console.Write("]\n");
+
+            // Testing Constructor with Pre-made Array
+            Console.WriteLine("\n#== Secondary Constructor Test ==#");
+            string[] sArr = new string[] { "z", "h", "c", "g", "e", "f", "i", "a"};
+            MyList<string> sList = new MyList<string>(sArr);
+            Console.WriteLine(sList);
+
+            // Simple Sort Test
+            Console.WriteLine("\n#== Simple Sort Test ==#");
+            list.SimpleSort();
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
